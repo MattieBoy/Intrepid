87b105587b1843e13365ae60cdea921546591069
MattieBoy <gramatterial@gmail.com>
2016-02-27 22:01:22 -0500

    Added test for the list not existing. It passed from previous test and implementation. I need to further understand how scala handles Nil, nil, None, and Nothing.

--- TrendKata/src/test/scala/MattieTrendSpec.scala
+++ TrendKata/src/test/scala/MattieTrendSpec.scala
@@ -5,4 +5,8 @@ class MattieTrendSpec extends FlatSpec with Matchers {
   "MattieTrend" should "return nil if list is empty" in {
     MattieTrend.calculateTrend(List[Int]()) should be(None)
   }
+
+  "MattieTrend" should "return nil if list does not exist" in {
+    MattieTrend.calculateTrend(Nil) should be(None)
+  }
 }
