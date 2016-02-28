580e3b7ef7937873f5c79385cd0a3afc8b9fb8f7
MattieBoy <gramatterial@gmail.com>
2016-02-27 22:57:57 -0500

    added tests for the three conditions for calculating the pairs.

--- TrendKata/src/main/scala/com/example/MattieTrend.scala
+++ TrendKata/src/main/scala/com/example/MattieTrend.scala
@@ -8,7 +8,10 @@ object MattieTrend {
       return Some(0.0)
     } else {
       return Some(10.0)
-      
     }
+  }
+  
+  def calculatePairs(p1: Double, p2: Double) : Double = {
+    if (p1 == 0 && p2 > 0) 1.0 else if (p1 == p2) 0.0 else ((p2 - p1) / p1)
   }
 }
