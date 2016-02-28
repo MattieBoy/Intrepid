a69c2fe9a8092960f8bfcc39f5afc196f9192fbf
MattieBoy <gramatterial@gmail.com>
2016-02-28 00:18:17 -0500

    removed redundant keyword and parenthesis. its a small commit, but I didn't want to end the night with changes hanging out locally.

--- TrendKata/src/main/scala/com/example/MattieTrend.scala
+++ TrendKata/src/main/scala/com/example/MattieTrend.scala
@@ -6,20 +6,20 @@ object MattieTrend {
     var result: Double = 0.0
     
     if (list.isEmpty) {
-      return None
+      None
     } else if (list.length == 1) {
-      return Some(0.0)
+      Some(0.0)
     } else {
       val slidPairs: List[List[Double]] = list.sliding(2).toList
       slidPairs.foreach{(pair: List[Double]) => 
         val pairValue: Double = calculatePairs(pair(0), pair(1))
         result = result + pairValue
       }
-      return Some(result)
+      Some(result)
     }
   }
   
   def calculatePairs(p1: Double, p2: Double) : Double = {
-    if (p1 == 0 && p2 > 0) 1.0 else if (p1 == p2) 0.0 else ((p2 - p1) / p1)
+    if (p1 == 0 && p2 > 0) 1.0 else if (p1 == p2) 0.0 else (p2 - p1) / p1
   }
 }
