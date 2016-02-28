e98d6edb5fecb444b2510a7f7ba15251c6693764
MattieBoy <gramatterial@gmail.com>
2016-02-27 22:32:20 -0500

    Added test to return zero when list length is 1

--- TrendKata/src/main/scala/com/example/MattieTrend.scala
+++ TrendKata/src/main/scala/com/example/MattieTrend.scala
@@ -1,11 +1,14 @@
 package com.example
 
 object MattieTrend {
-  def calculateTrend(list: List[Int]) : Option[Int] = {
+  def calculateTrend(list: List[Double]) : Option[Double] = {
     if (list.isEmpty) {
       return None
+    } else if (list.length == 1) {
+      return Some(0.0)
     } else {
-      return Some(10)
+      return Some(10.0)
+      
     }
   }
 }
--- TrendKata/src/test/scala/MattieTrendSpec.scala
+++ TrendKata/src/test/scala/MattieTrendSpec.scala
@@ -3,10 +3,15 @@ import com.example.MattieTrend
 
 class MattieTrendSpec extends FlatSpec with Matchers {
   "MattieTrend" should "return nil if list is empty" in {
-    MattieTrend.calculateTrend(List[Int]()) should be(None)
+    MattieTrend.calculateTrend(List[Double]()) should be(None)
   }
 
   "MattieTrend" should "return nil if list does not exist" in {
     MattieTrend.calculateTrend(Nil) should be(None)
+  }
+
+  "MattieTrend" should "return zero if list has only one element" in {
+    val list: List[Double] = List(2.0)
+    MattieTrend.calculateTrend(list) should be(Some(0.0))
   }
 }
