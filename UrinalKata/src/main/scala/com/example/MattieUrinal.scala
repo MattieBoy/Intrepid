c35a0a83ea2651d5867c7b0b6bd74e241075940d
MattieBoy <gramatterial@gmail.com>
2016-03-03 17:25:52 -0500

    initial project creation

---
+++ UrinalKata/src/main/scala/com/example/MattieUrinal.scala
@@ -0,0 +1,7 @@
+package com.example
+
+object MattieUrinal {
+  def main(args: Array[String]): Unit = {
+    println("MattieUrinal, world!")
+  }
+}
---
+++ UrinalKata/src/test/scala/MattieUrinalSpec.scala
@@ -0,0 +1,7 @@
+import org.scalatest._
+
+class MattieUrinalSpec extends FlatSpec with Matchers {
+  "MattieUrinal" should "have tests" in {
+    true should === (true)
+  }
+}
