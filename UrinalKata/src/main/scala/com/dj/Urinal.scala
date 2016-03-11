e863f37e74800eb22af3d0d3fa98f12bd2e71d5e
DJ Daugherty <ddaugher@gmail.com>
2016-03-10 22:39:01 -0500

    adding Urinal Specs and class

--- UrinalKata/src/main/scala/com/dj/Urinal.scala
+++ UrinalKata/src/main/scala/com/dj/Urinal.scala
@@ -1,27 +1,65 @@
 package com.dj
 
 class Urinal() {
-  def getScore():Int = {
-    if (_leftNeighbor != None && _rightNeighbor != None) return 1
-    0
-  }
-
-  private var _leftNeighbor = None : Option[Urinal]
-  private var _rightNeighbor = None : Option[Urinal]
+  private var _leftNeighbor: Option[Urinal] = None
+  private var _rightNeighbor: Option[Urinal] = None
   private var _position: Int = 0
   private var _status: UrinalStatus = Available
 
-  def hasLeftNeighbor() = _leftNeighbor != None
-  def hasRightNeighbor() = _rightNeighbor != None
-  def isAvailable() = status == Available
-
   def leftNeighbor = _leftNeighbor
-  def leftNeighbor_= (value:Urinal):Unit = _leftNeighbor = Option(value)
+
+  def leftNeighbor_=(value: Urinal): Unit = _leftNeighbor = Option(value)
+
   def rightNeighbor = _rightNeighbor
-  def rightNeighbor_= (value:Urinal):Unit = _rightNeighbor = Option(value)
+
+  def rightNeighbor_=(value: Urinal): Unit = _rightNeighbor = Option(value)
+
   def position = _position
-  def position_= (value:Int):Unit = _position = value
+
+  def position_=(value: Int): Unit = _position = value
+
   def status = _status
-  def status_= (value: UrinalStatus):Unit = _status = value
+
+  def status_=(value: UrinalStatus): Unit = _status = value
+
+  def isAvailable(): Boolean = {
+    if (isOccupied(this)) return false
+
+    if (isFirstPositionAndAvailable()) return true
+
+    if (isLastPositionAndAvailable()) return true
+
+    if (isMiddlePositionAndAvailable()) return false
+
+    true
+  }
+
+  private def isOccupied(urinal: Urinal): Boolean = {
+    urinal.status == Occupied
+  }
+
+  def hasLeftNeighbor(): Boolean = {
+    _leftNeighbor != None
+  }
+
+  def hasRightNeighbor(): Boolean = {
+    _rightNeighbor != None
+  }
+
+  private def isMiddlePositionAndAvailable(): Boolean = {
+    return (hasLeftNeighbor() && isOccupied(leftNeighbor get)) || (hasRightNeighbor() && isOccupied(rightNeighbor get))
+  }
+
+  private def isFirstPositionAndAvailable(): Boolean = {
+    !hasRightNeighbor() && isUrinalAvailable()
+  }
+
+  private def isLastPositionAndAvailable(): Boolean = {
+    !hasLeftNeighbor() && isUrinalAvailable()
+  }
+
+  private def isUrinalAvailable(): Boolean = {
+    status == Available
+  }
 
 }
--- UrinalKata/src/test/scala/com/dj/UrinalSpec.scala
+++ UrinalKata/src/test/scala/com/dj/UrinalSpec.scala
@@ -9,136 +9,78 @@ class UrinalSpec extends FlatSpec with Matchers {
     Available should === (urinal.status)
   }
 
-  "Urinal" should "return isAvailable true when created" in {
+  "Urinal" should "return properly assigned position when requested" in {
     val urinal: Urinal = new Urinal
-
-    true should === (urinal.isAvailable())
-  }
-
-  "Urinal" should "return Occupied status when set properly" in {
-
-    var urinal: Urinal = new Urinal
-    urinal.status = Occupied
-
-    Occupied should === (urinal.status)
-  }
-
-  "Urinal" should "return isAvailable false when created" in {
-    var urinal: Urinal = new Urinal
-    urinal.status = Occupied
-
-    false should === (urinal.isAvailable())
-  }
-
-  "Urinal" should "return position 0 upon initial creation" in {
-    val urinal: Urinal = new Urinal
-
-    0 should === (urinal.position)
-  }
-
-  "Urinal" should "return properly assigned position after setting" in {
-    var urinal: Urinal = new Urinal
     urinal.position = 1
 
     1 should === (urinal.position)
   }
 
   "Urinal" should "be unavailable when status is occupied" in {
-
-    var urinal: Urinal = new Urinal
+    val urinal: Urinal = new Urinal
     urinal.status = Occupied
 
     false should === (urinal.isAvailable())
-
   }
 
-  "Urinal" should "not have an empty left neighbor by default" in {
-
-    var urinal: Urinal = new Urinal
+  "Urinal" should "not have a left neighbor by default" in {
+    val urinal: Urinal = new Urinal
 
     false should === (urinal.hasLeftNeighbor())
   }
 
-  "Urinal" should "have a left neighbor when set" in {
+  "Urinal" should "not have a right neighbor by default" in {
+    val urinal: Urinal = new Urinal
 
-    var urinal: Urinal = new Urinal
+    false should === (urinal.hasRightNeighbor())
+  }
+
+  "Urinal" should "have left neighbor when created" in {
+    val urinal: Urinal = new Urinal
     urinal.leftNeighbor = new Urinal
 
     true should === (urinal.hasLeftNeighbor())
   }
 
-  "Urinal" should "not have an empty right neighbor by default" in {
-
-    var urinal: Urinal = new Urinal
-
-    false should === (urinal.hasRightNeighbor())
-  }
-
-  "Urinal" should "have a right neighbor when set" in {
-
-    var urinal: Urinal = new Urinal
+  "Urinal" should "have right neighbor when created" in {
+    val urinal: Urinal = new Urinal
     urinal.rightNeighbor = new Urinal
 
     true should === (urinal.hasRightNeighbor())
   }
 
-  "Urinal" should "return score of 0 when occupied" in {
+  "Urinal" should "be considered occupied when an available urinal, with an occupied left and right neighbor exists" in {
+    val urinal: Urinal = new Urinal
 
-    var urinal: Urinal = new Urinal
-    urinal.status = Occupied
-    urinal.leftNeighbor == None
-    urinal.rightNeighbor == None
+    var left: Urinal = new Urinal
+    left.status = Occupied
+    urinal.leftNeighbor = left
 
-    0 should === (urinal.getScore())
+    var right: Urinal = new Urinal
+    right.status = Occupied
+    urinal.rightNeighbor = right
+
+    false should === (urinal.isAvailable())
   }
 
-  "Urinal" should "return score of 1 when available, with occupied left and right" in {
+  "Urinal" should "be considered available when an available urinal, in first position, with an non-existent left neighbor and occupied right neighbor" in {
+    val urinal: Urinal = new Urinal
 
-    var urinal: Urinal = new Urinal
-    urinal.status = Available
+    var right: Urinal = new Urinal
+    right.status = Occupied
+    urinal.rightNeighbor = right
 
-    var leftUrinal: Urinal = new Urinal
-    leftUrinal.status = Occupied
-    urinal.leftNeighbor = leftUrinal
-
-    var rightUrinal: Urinal = new Urinal
-    rightUrinal.status = Occupied
-    urinal.rightNeighbor = rightUrinal
-
-    1 should === (urinal.getScore())
+    true should === (urinal.isAvailable())
   }
+
+  "Urinal" should "be considered available when an available urinal, in last position, with an occupied left neighbor and non-existent right neighbor" in {
+    val urinal: Urinal = new Urinal
+
+    var left: Urinal = new Urinal
+    left.status = Occupied
+    urinal.leftNeighbor = left
+
+    true should === (urinal.isAvailable())
+  }
+
 }
-
-//  def "an available urinal, in first position, with an non-existent left neighbor and occupied right neighbor should be considered available"() {
-//  given: "instantiated Urinal"
-//  def urinal = new Urinal()
-//  urinal.status = UrinalStatus.AVAILABLE
-//
-//  when: "left neighbor does not exist"
-//  urinal.leftNeighbor = null
-//
-//  and: "right neighbor is occupied"
-//  def right = new Urinal()
-//  right.status = UrinalStatus.OCCUPIED
-//  urinal.rightNeighbor = right
-//
-//  then: "urinal is considered available"
-//  urinal.isAvailable() == true
-//}
-//
-//  def "an available urinal, in last position, with an occupied left and non-existent right neighbor should be considered available"() {
-//  given: "instantiated Urinal"
-//  def urinal = new Urinal()
-//  urinal.status = UrinalStatus.AVAILABLE
-//
-//  when: "right neighbor does not exist"
-//  urinal.rightNeighbor = null
-//
-//  and: "left neighbor is occupied"
-//  def left = new Urinal()
-//  left.status = UrinalStatus.OCCUPIED
-//  urinal.leftNeighbor = left
-//
-//  then: "urinal is considered available"
-//  urinal.isAvailable() == true
-//}
