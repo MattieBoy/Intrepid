60ee400363f4143f3a0de00faedb67608ec46559
DJ Daugherty <ddaugher@gmail.com>
2016-03-23 15:07:02 -0400

    refactoring the rank name

--- UrinalKata/src/main/scala/com/mattie/Urinal.scala
+++ UrinalKata/src/main/scala/com/mattie/Urinal.scala
@@ -1,10 +1,10 @@
 package com.mattie
 
-class Urinal(_status: UrinalStatus, pRank: Int) {
+class Urinal(_status: UrinalStatus, _rank: Int) {
   var status: UrinalStatus = _status
   var leftNeighbor: Urinal = null
   var rightNeighbor: Urinal = null
-  var rank: Int = pRank
+  var rank: Int = _rank
 
   def isOccupied: Boolean = {
     Occupied == status
